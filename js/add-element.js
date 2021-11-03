c4a43298d080b261985b4e975b68cea4c1e083e2
Paytoncd <paytoncd@usca.edu>
2021-11-03 13:02:38 -0400

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -3,16 +3,22 @@
  */
 
 // get all the hot class elements- change them to cool class
-
+$(".hot").each(function(){
+  $(this).removeClass("hot");
+  $(this).addClass("cool");
+});
 // traverse the elements
-
+$('#one').next().next().text("milk");
 // add a new element by clicking the plus sign
-
+$('#todo').append('<li>candy</li>');
+$('#add').click(addElement);
 // before and after are for siblings
 // append and prepend are for parent
 
 function addElement() {
   // add a new element
+  $('#todo').append('<li>candy</li>');
+
   // add a input text box
   
   // whenever the user are done add the element
@@ -20,10 +26,23 @@ function addElement() {
 }
 
 // bind click with the event handler
-
+$('li').click(changeStyle);
 //  click the li element will change the changeStyle
 //  three style : complete, cool, hot
-function changeStyle() {}
+function changeStyle() {
+    if($(this).hasClass('cool')){
+      $(this).removeClass('cool');
+      $(this).addClass('complete');
+    }else if($(this).hasClass('hot')){
+      $(this).removeClass('hot');
+      $(this).addClass('complete');
+
+    }
+
+    
+
+}
+
 
 // delete complete element by clicking the trash can
 document.getElementById('remove').addEventListener('click', removeElement);
